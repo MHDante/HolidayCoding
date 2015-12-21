ff07c7f05e0c2e67960b2c2b1287bbf048143df9
Liam Atticus Clarke <liam.att.clarke@gmail.com>
2015-12-21 02:52:29 -0500

    shit happened

--- js/ipp/liam.js
+++ js/ipp/liam.js
@@ -1,13 +1,21 @@
 var liam = function(imageData) {
   var data = imageData.data;
-  var f = function(ii) {
-    return ii };
+  var f = function(data, index) {
+      var x = index % canvas.width;
+      var y = index / canvas.height;
+      var centerX = canvas.width / 2;
+      var centerY = canvas.height / 2 + 400;
+      var diffX = centerX - x;
+      var diffY = centerY - y;
+      var dist = Math.sqrt(diffX * diffX + diffY * diffY);
+      return data * (Math.sin((dist / 5) + imageData.t * 100) + 1);
+  };
 
   for (var i = 0; i < data.length; i += 4) {
-    data[i]   = f(data[i]);   // red
-    data[i+1] = f(data[i+1]); // green
-    data[i+2] = f(data[i+2]); // blue
-    data[i+3] = f(data[i+3]); // alpha
+    data[i]   = f(data[i], i);   // red
+    data[i+1] = f(data[i+1], i); // green
+    data[i+2] = f(data[i+2], i); // blue
+    data[i+3] = f(data[i+3], i); // alpha
   }
   return imageData;
 };
