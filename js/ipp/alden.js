0b88e894eb79b983ef3cdc18437cb2c06507caf6
Alden Ozburn <aldendino@gmail.com>
2015-12-21 01:05:05 -0500

    Semicolons

--- js/ipp/alden.js
+++ js/ipp/alden.js
@@ -1,7 +1,8 @@
-var alden = function(imageData) {
+/*var alden = function(imageData) {
   var data = imageData.data;
   var f = function(ii) {
-    return ii };
+    
+    return ii; };
 
   for (var i = 0; i < data.length; i += 4) {
     data[i]   = f(data[i]);   // red
@@ -10,28 +11,29 @@ var alden = function(imageData) {
     data[i+3] = f(data[i+3]); // alpha
   }
   return imageData;
-};
+};*/
 
 
-/*var alden = function(input) {
-   output = input
+var alden = function(input) {
+   output = input;
    for(var i = 0; i < output.length; i += 4) {
-     var rsum = 0
-     var gsum = 0
-     var bsum = 0
-     var rad = 10
-     var cnt = 0
+     var rsum = 0;
+     var gsum = 0;
+     var bsum = 0;
+     var rad = 10;
+     var cnt = 0;
      for(var j = -rad*4; j <= rad*4; j += 4) {
        if(j >= 0 && j < output.length) {
-         cnt++
-         rsum += input[j]
-         gsum += input[j+1]
-         bsum += input[j+2]
+         cnt++;
+         rsum += input[j];
+         gsum += input[j+1];
+         bsum += input[j+2];
        }
      }
-     output[i] = rsum / cnt
-     output[i+1] = gsum / cnt
-     output[i+2] = bsum / cnt
+     output[i] = rsum / cnt;
+     output[i+1] = gsum / cnt;
+     output[i+2] = bsum / cnt;
    }
-   return output
-}*/
+   //return output;
+   return input;
+};
