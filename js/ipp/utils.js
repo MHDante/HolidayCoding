4ee12f5e603e036ecc82e1760cf8532e72f58ed5
Dante Camarena <mhdcj@hotmail.com>
2015-12-21 00:58:59 -0500

    CallAll

---
+++ js/ipp/utils.js
@@ -0,0 +1,12 @@
+var Random = function(x){
+  return Math.floor((Math.random() * x) + 1);
+}
+var ShuffleArray = function(array) {
+for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+return array;
+}
