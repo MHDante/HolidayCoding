83da3561672f519a27c1c19cdcab9955f9bd9022
Zack Harris <zack.tibia@gmail.com>
2015-12-21 01:40:04 -0500

    shufflearray updated

--- js/ipp/utils.js
+++ js/ipp/utils.js
@@ -4,11 +4,12 @@ var Random = function(x){
   return Math.floor((Math.random() * x) + 1);
 }
 var ShuffleArray = function(array) {
-for (var i = array.length - 1; i > 0; i--) {
+  var newarr = array.slice();
+for (var i = newarr.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-}
-return array;
+    var temp = newarr[i];
+    newarr[i] = newarr[j];
+    newarr[j] = temp;
+  }
+  return newarr;
 }
