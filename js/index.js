f298442e4cbbc141c93c65369a21dcbf960d6f10
Zack Harris <zack.tibia@gmail.com>
2015-12-21 03:15:55 -0500

    checkboxes almost working

--- js/index.js
+++ js/index.js
@@ -11,8 +11,22 @@ var ctx = canvas.getContext('2d');
 var time = 0;
 var step = 50;
 
-var fns = [alden, dante, david, liam, matthew, zack2];
-fns = ShuffleArray(fns);
+var functionMap = {
+	'alden': { func: alden, checked: false },
+	'dante': { func: dante, checked: false },
+	'david': { func: david, checked: false },
+	'liam': { func: liam, checked: false },
+	'matthew': { func: matthew, checked: false },
+	'zack': { func: zack2, checked: false },
+};
+var checkedFuncs = 0;
+//var fns = [alden, dante, david, liam, matthew, zack2];
+var fns = [];
+for(var name in functionMap) {
+	fns.push(functionMap[name].func);
+}
+var shuffled = ShuffleArray(fns);
+var keysArray = new Array(fns.length);
 
 var loop = function() {
 
@@ -21,12 +35,58 @@ var loop = function() {
 	myImageData.t = time;
 	time += step/1000;
 
-	var length = fns.length;
+	var length = shuffled.length;
 	for (var ii = 0; ii < length; ii++) {
-		myImageData = fns[ii](myImageData);
+		myImageData = shuffled[ii](myImageData);
 	}
 	ctx.putImageData(myImageData, 0, 0);
 
 	setTimeout(loop, time);
 };
 img.onload = loop;
+
+function tableCreate() {
+    var body = document.getElementsByTagName('body')[0];
+    var tbl = document.createElement('table');
+    tbl.style.width = '120px';
+    tbl.setAttribute('border', '1');
+    var tbdy = document.createElement('tbody');
+		var keys = Object.keys(functionMap);
+    for (var i = 0; i < keys.length; i++) {
+        var tr = document.createElement('tr');
+        for (var j = 0; j < 2; j++) {
+              var td = document.createElement('td');
+							if (j == 0) {
+								var key = keys[i];
+              	td.appendChild(document.createTextNode(key));
+							} else if (j == 1) {
+								var checkbox = document.createElement('input');
+								checkbox.type = "checkbox";
+								checkbox.name = "name";
+								checkbox.value = "value";
+								checkbox.id = "checkbox-id";
+								td.appendChild(checkbox);
+								td.onclick = (function(funcName) {
+									return function() {
+										var prevChecked = functionMap[funcName].checked;
+										if (prevChecked != this.checked) {
+											if (this.checked) {
+												checkedFuncs += 1;
+											} else {
+												checkedFuncs -= 1;
+											}
+											functionMap[funcName].checked = this.checked;
+										}
+									}
+								})();
+
+
+							}
+              tr.appendChild(td);
+        }
+        tbdy.appendChild(tr);
+    }
+    tbl.appendChild(tbdy);
+    body.appendChild(tbl)
+};
+tableCreate();
